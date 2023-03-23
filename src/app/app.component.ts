0c467dd0d03da05cb1ced1b11a9c8f488f515c8e
QUADIONTECH\ggarcia <ggarcia@quadiontech.com>
2023-03-23 10:47:54 -0300

    Drag and drop in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-drag-and-drop';
+    todos = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+    done = ["Item 6", "Item 7"];
+
+    dropped(event: CdkDragDrop<string[]>) {
+        if (event.previousContainer === event.container) {
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        } else {
+            transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex)
+        }
+    }
 }
